5dc1c1099fb4994f6747b6675a85cc5b4a2a9e8a
Fabian Sturm <f@rtfs.org>
2015-11-12 19:33:54 +0100

    Fix typo.
    Change-Id: I9afa212587e6004097285048abc78fb1252bfc3f

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@ import pytest
 from regatta import main
 
 def test_main():
-  # This test does not make sense, it's just to check the coeverage framework
+  # This test does not make sense, it's just to check the coverage framework
   assert(main.main()==None)
