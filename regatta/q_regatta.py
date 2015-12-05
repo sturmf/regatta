12fc1ffc60071a48fe8a74b99154d6c80399e203
Fabian Sturm <f@rtfs.org>
2015-12-05 16:17:31 +0100

    Fix super call.
    Change-Id: I8d36ff0bb1ed7f162846228f8e94ba02d582bd2d

--- regatta/q_regatta.py
+++ regatta/q_regatta.py
@@ -6,7 +6,7 @@ from regatta import Regatta
 class QRegatta(QObject):
 
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super(QRegatta, self).__init__(parent)
         self._filename = None
         self._regatta = None
 
