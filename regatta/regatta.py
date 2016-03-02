06945ac31726ee50b3841ab39861caf70689cf9f
Fabian Sturm <f@rtfs.org>
2016-03-02 18:52:00 +0100

    Set proper default values for the Event.
    Change-Id: If094ba8c6165e7c88a37b64d8616a5924b61d75f

--- regatta/regatta.py
+++ regatta/regatta.py
@@ -42,21 +42,28 @@ class Event(Base):
 
     id = Column(Integer, primary_key=True)
     """name of the event"""
-    name = Column(String, nullable=False, default='')
+    name = Column(String, nullable=False)
     """kind of the event either Class or Yardstick"""
-    mode = Column(Enum(*[x.name for x in Regatta.Mode], name='mode_types'), default=Regatta.Mode.Class.name)
+    mode = Column(Enum(*[x.name for x in Regatta.Mode], name='mode_types'))
     """first day of the event"""
-    start_date = Column(Date, default=datetime.datetime.utcnow)
+    start_date = Column(Date)
     """last day of the event"""
-    end_date = Column(Date, default=datetime.datetime.utcnow)
+    end_date = Column(Date)
     """amount of races scheduled during the event"""
-    race_count = Column(Integer, default=1)
+    race_count = Column(Integer)
     """the sailing club that organizes the event"""
     organizer_id = Column(Integer, ForeignKey('sailing_club.id'))
     organizer = relationship("SailingClub")
     """the person responsible for the execution of the event"""
     race_committee_id = Column(Integer, ForeignKey('person.id'))
     race_committee = relationship("Person")
+
+    def __init__(self):
+        self.name = ''
+        self.mode = Regatta.Mode.Yardstick.name
+        self.start_date = datetime.datetime.utcnow()
+        self.end_date = datetime.datetime.utcnow()
+        self.race_count = 1
 
 
 class SailingClub(Base):
